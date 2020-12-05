c220ffa2064df87dfea50b134445f63b36084e85
Petr Krátký <petr.kratky@ibm.com>
2020-12-05 18:31:54 +0100

    fix express.listen() wrong func overload

--- reality-backend-server/src/server.ts
+++ reality-backend-server/src/server.ts
@@ -75,11 +75,7 @@ export class ApiServer {
    */
   public async start(): Promise<ApiServer> {
     return new Promise<ApiServer>((resolve, reject) => {
-      this.server = this.app.listen(this.PORT, (err: any) => {
-        if (err) {
-          return reject(err);
-        }
-
+      this.server = this.app.listen(this.PORT, () => {
         const addressInfo = this.server.address() as AddressInfo;
 
         const address = addressInfo.address === '::' ? 'localhost' : addressInfo.address;
@@ -88,7 +84,8 @@ export class ApiServer {
         console.log(`Listening to http://${address}:${addressInfo.port}`);
 
         return resolve(this);
-      });
+      })
+      .on("error", (err) => reject(err));
     });
   }
 
