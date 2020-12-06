a64ec5292e9c7550eb90c2b8e9bdc0fedcff26c2
Petr Krátký <petr.kratky@ibm.com>
2020-12-06 12:09:26 +0100

    formatting

--- reality-frontend/src/server/Server.ts
+++ reality-frontend/src/server/Server.ts
@@ -1,11 +1,10 @@
-import express, { Express } from 'express';
+import express, { Express } from 'express'
 import path from 'path'
-import next from 'next';
+import next from 'next'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import { createProxyMiddleware, RequestHandler } from 'http-proxy-middleware'
-
 
 async function startServer(): Promise<{ protocol: string; port: number; host: string }> {
   const graphqlUrl: string = process.env.GRAPHQL_SERVER || 'http://localhost:4000/graphql'
@@ -17,22 +16,22 @@ async function startServer(): Promise<{ protocol: string; port: number; host: st
   const port: number = +process.env.PORT || 3000
   const dev: boolean = process.env.NODE_ENV !== 'production'
 
-  const server: Express = express();
-  const app = next({ dev: dev });
-  await app.prepare();
+  const server: Express = express()
+  const app = next({ dev: dev })
+  await app.prepare()
 
-  const nextMiddleware = app.getRequestHandler();
+  const nextMiddleware = app.getRequestHandler()
 
   const refreshProxy: RequestHandler = createProxyMiddleware('/api/refresh_token', {
     target: refreshUrl,
-    onError: err => console.error(err),
-    pathRewrite: {'^/api/refresh_token': '/refresh_token'}
+    pathRewrite: { '^/api/refresh_token': '/refresh_token' },
+    onError: err => console.error(err)
   })
 
   const graphqlProxy: RequestHandler = createProxyMiddleware('/api/graphql', {
     target: graphqlUrl,
-    onError: err => console.error(err),
-    pathRewrite: {'/api/graphql': 'graphql'}
+    pathRewrite: { '/api/graphql': 'graphql' },
+    onError: err => console.error(err)
   })
 
   const postgisProxy: RequestHandler = createProxyMiddleware('/api/postgis', {
@@ -47,26 +46,21 @@ async function startServer(): Promise<{ protocol: string; port: number; host: st
   server.use(postgisProxy)
   server.use(refreshProxy)
 
-  server.get('/health', (req, res) => res.send('UP!'));
-  server.get('*', (req, res) => nextMiddleware(req, res));
+  server.get('/health', (req, res) => res.send('UP!'))
+  server.get('*', (req, res) => nextMiddleware(req, res))
 
   server.listen(port, host)
 
   return { protocol, host, port }
 }
 
-
 function loadEnv(): void {
-  const envPath =
-    process.env.NODE_ENV === "production"
-      ? ".env.server.prod"
-      : ".env.server.dev"
+  const envPath = process.env.NODE_ENV === 'production' ? '.env.server.prod' : '.env.server.dev'
   dotenv.config({ path: path.join('./env', envPath) })
 }
-
 
 loadEnv()
 
 startServer()
   .then(({ protocol, host, port }) => console.log(`Next.js is ready on ${protocol}://${host}:${port}`))
-  .catch((err) => console.error(`Server failed to start:\n${err}`))
+  .catch(err => console.error(`Server failed to start:\n${err}`))
