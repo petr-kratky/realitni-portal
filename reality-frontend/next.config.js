7e7469d56fa0b0a72ac98088e6923a2aec8876c9
Petr Krátký <petr.kratky@ibm.com>
2021-08-31 21:27:54 +0200

    remove hard coded env vars

--- reality-frontend/next.config.js
+++ reality-frontend/next.config.js
@@ -1,12 +1,6 @@
-const path = require('path')
-const withCustomBabelConfigFile = require('next-plugin-custom-babel-config')
+const path = require("path")
+const withCustomBabelConfigFile = require("next-plugin-custom-babel-config")
 
 module.exports = withCustomBabelConfigFile({
-  babelConfigFile: path.resolve('babel.config.js'),
-  env: {
-    NEXT_PUBLIC_GOOGLE_API_KEY: 'AIzaSyBqYOqXDwG5_i6bFv0I2agVONDfl0KJ8uw',
-    NEXT_PUBLIC_GOOGLE_GEOCODE_URL: 'https://maps.googleapis.com/maps/api/geocode/json',
-    NEXT_PUBLIC_MAPBOX_TOKEN:
-      'pk.eyJ1IjoicGtyYXRreSIsImEiOiJjazJkcHNpaDcwMThpM21vNGZqNTZsZDQxIn0.QC03U0mLmV9AyM1tuyI-gg'
-  }
+  babelConfigFile: path.resolve("babel.config.js")
 })
