410de8beeb60b4cbe50501ae7f3b694cc5d181ee
Petr Krátký <petr.kratky@ibm.com>
2021-09-06 21:10:51 +0200

    reusable estate menu popover

---
+++ reality-frontend/src/components/estate/EstateMenu.tsx
@@ -0,0 +1,50 @@
+import React from "react"
+
+import { createStyles, ListItemIcon, ListItemText, makeStyles, Menu, MenuItem, Theme } from "@material-ui/core"
+import EditIcon from "@material-ui/icons/Edit"
+import DeleteIcon from "@material-ui/icons/Delete"
+
+type ComponentProps = {
+  open: boolean
+  menuAnchor: null | HTMLElement
+  onClose: () => void
+  onEditClick: () => void
+  onDeleteClick: () => void
+}
+
+const useStyles = makeStyles((theme: Theme) => createStyles({}))
+
+const Component: React.FunctionComponent<ComponentProps> = ({
+  open,
+  menuAnchor,
+  onClose,
+  onEditClick,
+  onDeleteClick
+}) => {
+  const classes = useStyles()
+
+  return (
+    <Menu
+      anchorEl={menuAnchor}
+      elevation={2}
+      open={open}
+      onClose={onClose}
+      transformOrigin={{ vertical: "top", horizontal: "left" }}
+    >
+      <MenuItem onClick={onEditClick}>
+        <ListItemIcon>
+          <EditIcon />
+        </ListItemIcon>
+        <ListItemText primary='Upravit' />
+      </MenuItem>
+      <MenuItem onClick={onDeleteClick}>
+        <ListItemIcon>
+          <DeleteIcon />
+        </ListItemIcon>
+        <ListItemText primary='Smazat' />
+      </MenuItem>
+    </Menu>
+  )
+}
+
+export default Component
